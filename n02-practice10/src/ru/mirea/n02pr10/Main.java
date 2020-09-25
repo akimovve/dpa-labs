db9c87b7bdb555bf013f91c172e37de92ff0ddc1
_akimovve <valery.e.akimov@gmail.com>
2020-09-25 17:24:59 +0300

    Update practice 2

--- n02-practice10/src/ru/mirea/n02pr10/Main.java
+++ n02-practice10/src/ru/mirea/n02pr10/Main.java
@@ -1,14 +1,17 @@
 package ru.mirea.n02pr10;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Тестирование деревьев...");
+        System.out.println("Тестирование деревьев...");
         BinaryTree<Integer> binaryTree = new BinaryTree<>();
         Tree<Integer> avlTree = new AvlTree<>();
+        System.out.print("Бинарное дерево поиска. Введите количество узлов: ");
         test(binaryTree);
+        System.out.print("AVL-дерево. Введите количество узлов: ");
         test(avlTree);
         binaryTree.getSortedArray().forEach(System.out::println);
     }
@@ -16,14 +19,21 @@ public class Main {
     private static void test(Tree<Integer> tree) {
         //int[] testArray = {11, 25, 21, 6, 4, 9, 67, 33, 6};
         //int[] testArray = {1, 25, 26, 27, 4, 67, 33};
-        int[] testArray = {6, 12, 14, 13, 16, 2, 3, 1, 25, 26, 10, 4, 20, 7};
-        Arrays.stream(testArray).forEach(tree::addNode);
-        tree.addNode(15, 67, 1);
-        System.out.println("\n\n\nИзначальное дерево...\n");
+        //int[] testArray = {6, 12, 14, 13, 16, 2, 3, 1, 25, 26, 10, 4, 20, 7};
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        for (int i = 0; i < len; i++) {
+            tree.addNode(scanner.nextInt());
+            tree.print();
+        }
+
+        //Arrays.stream(testArray).forEach(tree::addNode);
+        //tree.addNode(15, 67, 1);
+        //System.out.println("\n\n\nИзначальное дерево...\n");
         tree.print();
-        tree.remove(6);
-        tree.remove(2);
-        System.out.println("\n\nПосле удаления элементов...\n");
-        tree.print();
+        //tree.remove(6);
+        //tree.remove(2);
+        //System.out.println("\n\nПосле удаления элементов...\n");
+        //tree.print();
     }
 }
